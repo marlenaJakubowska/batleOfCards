37b583dabb108575634c63071917f63d4e89e609
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-05-05 12:00:35 +0200

    fix

--- src/com/codecool/battleofcards/Game.java
+++ src/com/codecool/battleofcards/Game.java
@@ -4,10 +4,5 @@ public class Game {
 
     public void init() {
         System.out.println("hello");
-//        System.out.println("How many AI you want to play with?");
-//        scanner howmany = new Scanner(System.in);
-//        boolean gameOn = true;
-//        while (gameOn == true){
-//        }
     }
 }
