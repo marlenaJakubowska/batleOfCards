96c82d58a8e060d197bf2ba6f798d220921413bb
Huberkrok <hubh1234@wp.pl>
2020-05-05 11:06:26 +0200

    Table add

---
+++ src/com/codecool/battleofcards/Table.java
@@ -0,0 +1,16 @@
+package com.codecool.battleofcards;
+
+public class Table {
+
+    private List<Card> listOfCards;
+    private List<CurrentCards> currentCards;
+    
+    public List<tableCards> tableListOfCards(){
+        return listOfCards;
+    }
+
+    public List<Card> tableCurrentCards(){
+        return currentCards;
+    }
+
+}
