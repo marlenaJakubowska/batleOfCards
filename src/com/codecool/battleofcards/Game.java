6cb421556ce988f53f33e15781dd3e0d9c7101f5
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-05-05 11:49:25 +0200

    fix missing imports

--- src/com/codecool/battleofcards/Game.java
+++ src/com/codecool/battleofcards/Game.java
@@ -1,16 +1,14 @@
 package com.codecool.battleofcards;
 
-import jdk.nashorn.internal.parser.Scanner;
-
 public class Game {
 
     public void init() {
         System.out.println("hello");
-        System.out.println("How many AI you want to play with?");
-        scanner howmany = new Scanner(System.in);
-        boolean gameOn = true;
-        while (gameOn == true){
+//        System.out.println("How many AI you want to play with?");
+//        scanner howmany = new Scanner(System.in);
+//        boolean gameOn = true;
+//        while (gameOn == true){
             
-        }
+//        }
     }
 }
--- src/com/codecool/battleofcards/Table.java
+++ src/com/codecool/battleofcards/Table.java
@@ -1,9 +1,11 @@
 package com.codecool.battleofcards;
 
+import java.util.List;
+
 public class Table {
 
     private List<Card> listOfCards;
-    private List<Cards> currentCards;
+    private List<Card> currentCards;
     
     public List<Card> tableListOfCards(){
         return listOfCards;
