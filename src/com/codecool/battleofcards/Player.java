1a04508e04fc5bea52db6ef2312f407d11a728c3
Mat191126 <matprz11@gmail.com>
2020-05-05 10:36:09 +0200

    Added Player methods

--- src/com/codecool/battleofcards/Player.java
+++ src/com/codecool/battleofcards/Player.java
@@ -4,4 +4,20 @@ public class Player {
     private String name;
     private List<Card> listOfCards;
     private Card topcard;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Card> getListOfCards(){
+        return listOfCards;
+    }
+
+    public Card getTopcard(){
+        return topcard;
+    }
 }
