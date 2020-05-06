6409b91055698f4005e28b4709eba539156762e0
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-05-06 18:58:20 +0200

    add shuffle cards to table, move  card reader initialisation to table

--- src/com/codecool/battleofcards/Game.java
+++ src/com/codecool/battleofcards/Game.java
@@ -5,19 +5,12 @@ import java.util.Arrays;
 public class Game {
 
     public void init() {
-        CardReader cardReader = new CardReader();
-        System.out.println("List of cards:");
-        printCards(cardReader.getDeck());
+        Table table = new Table();
+
+
     }
 
-    public void printCards(Deck deck) {
-        for (Card card : deck.getCards()) {
-            System.out.println(card.getCardName());
-            System.out.println(card.getCardStats());
-            System.out.println("value by criticScore: " + card.getValueByType("criticScore"));
-            System.out.println("__________________________-");
-        }
-    }
+
 }
 
 
--- src/com/codecool/battleofcards/Table.java
+++ src/com/codecool/battleofcards/Table.java
@@ -1,5 +1,6 @@
 package com.codecool.battleofcards;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Table {
@@ -14,5 +15,27 @@ public class Table {
     public List<Card> tableCurrentCards(){
         return currentCards;
     }
+
+    public Table(){
+        CardReader cardReader = new CardReader();
+        System.out.println("List of cards:");
+        printCards(cardReader.getDeck());  // test
+        shuffleCards(cardReader.getDeck()); // test
+        printCards(cardReader.getDeck()); // test
+    }
+
+    public void shuffleCards(Deck deck) {
+        Collections.shuffle(deck.getCards());
+    }
+
+    public void printCards(Deck deck) {  //test
+        for (Card card : deck.getCards()) {
+            System.out.println(card.getCardName());
+            System.out.println(card.getCardStats());
+            System.out.println("value by criticScore: " + card.getValueByType("criticScore"));
+            System.out.println("__________________________-");
+        }
+    }
+
 
 }
